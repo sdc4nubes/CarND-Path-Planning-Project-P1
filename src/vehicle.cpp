aeb1a225c7928d6d3872b432e8ac28d9fc4dcfd0
rons-git <ron@cancunron.com>
2019-03-08 14:33:43 -0500

    update from windows

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -94,7 +94,7 @@ int VehiclePlanner::laneCost(double s, int lane, vector<vector<double>> sensor_f
     avg_costs[i] = (avg_costs[i] * 9) + costs[i];
     avg_costs[i] /= 10;
   }
-	cout << avg_costs << endl;
+	for (auto x = end(avg_costs); x != begin(avg_costs);) cout << *--x << ' ';
   // Evaluate potential lane change based on lowest cost
   if (lane == 0) {
     return min_element(avg_costs.begin(), avg_costs.end() - 1) - avg_costs.begin();
