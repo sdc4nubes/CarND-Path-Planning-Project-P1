632de24499f7871db1c3e90fc38cdfffda0de8d2
rons-git <ron@cancunron.com>
2019-03-09 01:22:34 -0500

    update from windows

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -11,8 +11,6 @@ int VehiclePlanner::lanePlanner(double s, double d, vector<vector<double>> senso
   if (distance > safe_distance && lane != 2) {
     new_lane = lane;
     target_vehicle_speed = speed_limit;
-		// Reset average costs for laneCost()
-    avg_costs = {15, 15, 15}; 
     return 0;
   } else {
 		// Determine new lane based on cost model
