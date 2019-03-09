a7614a227b155b1e02e3d2da4e212bec4faacde1
rons-git <ron@cancunron.com>
2019-03-08 21:10:19 -0500

    update from windows

--- src/main.cpp
+++ src/main.cpp
@@ -142,7 +142,7 @@ int main() {
 						double move = vp.lanePlanner(frenet_vec[0], frenet_vec[1], sensor_fusion);
             double lane = vp.curr_lane;
             double next_d = (lane * 4) + 2 + move;
-						int horizon = 40;
+						int horizon = 50;
 						if (car_speed <= vp.speed_limit * .8) horizon = 45;
             // Set further waypoints based on going further along highway in desired lane
             vector <double> wp1 = getXY(car_s + horizon, next_d, map_waypoints_s, 
@@ -174,7 +174,7 @@ int main() {
               double target_y = s(target_x);
               double target_dist = sqrt(pow(target_x, 2) + pow(target_y, 2));
               double x_add_on = 0;
-              const int MAX_ACCEL = 3;
+              const int MAX_ACCEL = 4;
 							// Limit acceleration to prevent jerk
               const double accel = MAX_ACCEL * 0.02;
               for(int i = 0; i < 50 - path_size; i++) {
