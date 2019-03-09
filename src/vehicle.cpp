4367dce6adafe01b58bd337bb154d69c30b9c08b
rons-git <ron@cancunron.com>
2019-03-09 11:38:55 -0500

    update from windows

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -91,9 +91,9 @@ int VehiclePlanner::laneCost(double s, int lane, vector<vector<double>> sensor_f
 		// Prohibitive cost for vehicle ahead too close
     if (i != lane && front_vehicle[0] < safe_distance) costs[i] = 15; 
 		// Positive cost for slower vehicle in front
-		if (front_vehicle[0] < safe_distance * 10) {
+		if (front_vehicle[0] < safe_distance * 5) {
 			if (front_vehicle[1] < check_speed) costs[i] = 15;
-			if (front_vehicle[1] < speed_limit) costs[i] += 10;
+			if (front_vehicle[1] < speed_limit) costs[i] += 5;
 		}
 		if (lane != i) {
 			if ((back_vehicle[0] < safe_distance) ||
