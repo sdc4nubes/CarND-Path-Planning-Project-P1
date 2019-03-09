b96b60a4b9ee2b9a62ba2be1501cfa6428221ab9
rons-git <ron@cancunron.com>
2019-03-08 22:43:48 -0500

    update from windows

--- src/main.cpp
+++ src/main.cpp
@@ -142,7 +142,7 @@ int main() {
 						double move = vp.lanePlanner(frenet_vec[0], frenet_vec[1], sensor_fusion);
             double lane = vp.curr_lane;
 						double diff = lane * 4 - car_d;
-            double next_d = (lane * 4) + 2 + move + diff;
+            double next_d = (lane * 4) + 2 + move - diff;
 						int horizon = 50;
 						if (car_speed <= vp.speed_limit * .8) horizon = 20;
             // Set further waypoints based on going further along highway in desired lane
