1a57975999f5042e366aef9ad0a394077dfe76b8
rons-git <ron@cancunron.com>
2019-03-10 01:29:53 -0500

    update from windows

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -17,7 +17,7 @@ int VehiclePlanner::lanePlanner(double s, double d, vector<vector<double>> senso
     new_lane = laneCost(s, lane, sensor_fusion);
     vector <double> vehicle = closestVehicle(s, new_lane, sensor_fusion, true);
 		target_vehicle_speed = speed_limit_mps;
-    if( vehicle[0] < unsafe_distance) target_vehicle_speed = vehicle[1];
+    if( vehicle[0] < unsafe_distance) target_vehicle_speed = vehicle[1] *.5;
 		if (vehicle[0] < unsafe_distance * .5) target_vehicle_speed *= .1;
   }
   // Return New Lane (0 = stay in lane, -4 = change left, 4 = change right)
