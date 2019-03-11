1dafbc10b89396ff2d229c5252fb17be8cc370dc
rons-git <ron@cancunron.com>
2019-03-10 20:22:28 -0400

    update from windows

--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 git add -A
 git commit -m "update from windows"
-git push https://github.com/rons-git/CarND-Path-Planning-Project-P1.git master
+git push https://github.com/sdc4nubes/CarND-Path-Planning-Project-P1.git master
 rem set /p=
